6894391ea2488cea20bc645b41df70547914b3cc
POURIA <p.khadivar@gmail.com>
2023-05-06 17:24:55 +0430

    Implement context for globl state management and cart page

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import { useContext } from "react";
+import { CartContext } from "../context/Cart";
+
 const Layout = ({ title, children }) => {
+  const { state, dispatch } = useContext(CartContext);
+  const { cart } = state;
   return (
     <>
       <Head>
@@ -11,17 +16,27 @@ const Layout = ({ title, children }) => {
         <header>
           <nav className="flex h-14 px-8 justify-between items-center border-b-4 bg-white">
             <Link legacyBehavior className="text-lg font-bold" href="/">
-               Shopping
+              Shopping
             </Link>
             <div>
-              <Link className="p-2" href="/cart">Cart</Link>
-              <Link className="p-2" href="/login">Login</Link>
-              
+              <Link className="p-2" href="/cart">
+                Cart
+                {cart.cartItems.lenght > 0 && (
+                  <span clasName="ml-1 rounded-xl bg-gray-200 px-2 py-1 text-xs font-bold">
+                    {cart.cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </span>
+                )}
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex justify-center items-center h-10">Footer</footer>
+        <footer className="flex justify-center items-center h-10">
+          Footer
+        </footer>
       </div>
     </>
   );
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,16 @@
 import { useRouter } from "next/router";
-import Layout from "../../components/Layout";
-import ProductItems from "../../data/products.json";
 import Image from "next/image";
 
+import { useContext } from "react";
+import { CartContext } from "../../context/Cart";
+
+import Layout from "../../components/Layout";
+import ProductItems from "../../data/products.json";
+
 const ProductPage = () => {
+  const { state, dispatch } = useContext(CartContext);
+
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
 
@@ -11,10 +18,27 @@ const ProductPage = () => {
 
   if (!product) return <h2>There is no product</h2>;
 
+  const addToCartHandler = () => {
+    const existingItem = state.cart.cartItems.find(
+      (item) => item.slug === product.slug
+    );
+    const qty = existingItem ? existingItem.qty + 1 : 1;
+
+    if (product.count < qty) {
+      alert("Product is out.");
+
+      return;
+    }
+
+    dispatch({ type: "ADD_TO_CART", payload: { ...product, qty } });
+
+    router.push("/cart")
+  };
+
   return (
     <Layout title={product.title}>
       <div className="grid md:grid-cols-4 md:gap-3 bg-white rounded-xl p-10">
-        <div>
+        <div className="md:cols-span-2">
           <Image
             className="rounded-xl"
             src={product.image}
@@ -37,11 +61,14 @@ const ProductPage = () => {
           </div>
           <div className="mb-2 flex justify-between">
             <div>Status:</div>
-            <div>
-                {product.count > 0 ? "Available" : "Unavailble"}
-            </div>
+            <div>{product.count > 0 ? "Available" : "Unavailble"}</div>
           </div>
-          <button className="rounded-xl bg-gray-700 text-white px-4 py-2 w-full">Add to cart</button>
+          <button
+            onClick={addToCartHandler}
+            className="rounded-xl bg-gray-700 text-white px-4 py-2 w-full"
+          >
+            Add to cart
+          </button>
         </div>
       </div>
     </Layout>
